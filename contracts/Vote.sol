10636813f5c2005f3ee10cfff70814271b465d0d
cha0sg0d <tony.goss@protonmail.com>
2021-11-30 19:00:19 -0500

    allow for multiple proposals on contract

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -3,16 +3,55 @@ pragma solidity 0.8.4;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
+contract Vote is Ownable { 
+  uint256 public numProposals;
+  uint256 public numOptions;
 
-contract Vote is Ownable {
-      
-  mapping(uint256 => string) public proposals;
-  mapping (uint256 => uint256) public votes;
-  mapping (address => bool) public voted;
+  struct UserOption {
+    string name;
+    string description;
+    string uri;
+  }
 
-  uint256 public proposalCounter;
+  struct Option {
+    uint256 id;
+    uint256 proposalId;
+    uint256 votes;
+    string name;
+    string description;
+    string uri;
+  }
+
+  struct UserProposal {
+    address owner;
+    uint256 id;
+    string name;
+    string uri;
+    uint256[] optionIds;
+    uint256 numVoters;
+  }
+
+  struct Proposal {
+    address owner;
+    uint256 id;
+    string name;
+    string uri;
+    uint256[] optionIds;
+    uint256 numVoters;
+    /* voter -> optionId */
+    mapping (address => uint256) votes;
+    /* voterId -> voter */
+    mapping (uint256 => address) voters;
+  }
+
+  mapping (uint256 => Proposal) public proposals;
+  mapping (uint256 => Option) public options;
+
+  constructor () {
+    /* set numOptions to 1 so 0 can represent a no vote */
+    numOptions = 1;
+  }
 
   modifier isWhitelisted() {
     // TODO: add real whitelist gating
@@ -20,25 +59,72 @@ contract Vote is Ownable {
     _;
   }
 
-  function createProposal (string calldata uri) external onlyOwner {
-    proposals[proposalCounter] = uri;
-    proposalCounter++;
+  modifier notVoted(uint256 _proposalId) {
+    Proposal storage proposal = proposals[_proposalId];
+    require(proposal.votes[msg.sender] == 0, "already voted");
+    _;
   }
 
-  function vote (uint256 proposalId) public isWhitelisted {
-    // require(voted[msg.sender] == false, "Already voted");
-
-    votes[proposalId]++;
-    voted[msg.sender] = true;
+  modifier onlyProposalOwner(uint256 _proposalId){
+    require(proposals[_proposalId].owner == msg.sender, "not proposal owner");
+    _;
   }
 
-  // function getProposals() public view returns (string [] memory)  {
-  //   string[] memory proposalArr = new string[](proposalCounter);
-  //   for (uint256 index = 0; index < proposalCounter; index++) {
-  //     proposalArr[index] = proposals[index];
-  //   }
-  //   return proposalArr;
-  // }
+  function createProposal (string calldata _name, string calldata _uri, UserOption[] calldata _options) external onlyOwner {
+    /* moment where proposal is allocated to storage */
+    Proposal storage proposal = proposals[numProposals];
+    proposal.owner = msg.sender;
+    proposal.id = numProposals;
+    proposal.name = _name;
+    proposal.uri = _uri;
+    /* This can be a function and be cleaner */
+    for (uint256 index = 0; index < _options.length; index++) {
+      UserOption calldata currOption = _options[index];
+      Option storage option = options[numOptions];
+      option.id = numOptions;
+      option.proposalId = numProposals;
+      option.name = currOption.name;
+      option.description = currOption.description;
+      option.uri = currOption.uri;
+      /* Add to proposal list */
+      proposal.optionIds.push(numOptions);
+      numOptions++;
+    }
+
+    numProposals++;
+  }
+
+  function getProposal (uint256 _proposalId) public view returns (UserProposal memory) {
+    Proposal storage proposal = proposals[_proposalId];
+    return UserProposal(
+      proposal.owner,
+      proposal.id,
+      proposal.name,
+      proposal.uri,
+      proposal.optionIds,
+      proposal.numVoters
+    );
+  }
+
+  function getOptions(uint256[] memory _optionIds) public view returns (Option[] memory) {
+    Option[] memory tempOptions = new Option[](_optionIds.length);
+    for (uint256 index = 0; index < _optionIds.length; index++) {
+      tempOptions[index] = options[_optionIds[index]];
+    }
+    return tempOptions;
+  }
+
+  function vote (uint256 _proposalId, uint256 _optionId) public isWhitelisted notVoted(_proposalId)  {
+    Option storage option = options[_optionId];
+    Proposal storage proposal = proposals[_proposalId];
+    /* increment vote for proposal */
+    option.votes++;
+    /* store vote of msg.sender */
+    proposal.votes[msg.sender] = _optionId;
+    /* update getter to look up voter's choice later on */
+    proposal.voters[proposal.numVoters] = msg.sender;
+    proposal.numVoters++;
+  }
 
   function terminate() public onlyOwner {
     selfdestruct(payable (owner()));
---
+++ test/vote.ts
@@ -0,0 +1,124 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+// import { SignerWithAddress } from "ethers";
+import { Vote } from "../typechain";
+
+const SAMPLE_PROPOSAL = [
+  {
+    name: "a",
+    description: "b",
+    uri: "https://google.com",
+  },
+  {
+    name: "b",
+    description: "c",
+    uri: "https://google.com",
+  },
+];
+
+describe("Vote", function () {
+  let vote: Vote;
+  // let owner, user1: any
+
+  describe("Test proposal creation", function () {
+    let user1: SignerWithAddress;
+    let owner: SignerWithAddress;
+    beforeEach(async function () {
+      [owner, user1] = await ethers.getSigners();
+      const Vote = await ethers.getContractFactory("Vote");
+      vote = (await Vote.deploy()) as Vote;
+      await vote.deployed();
+    });
+
+    it("owner can create proposal with no options", async function () {
+      const createTx = await vote.createProposal(
+        "test",
+        "testing out proposal",
+        []
+      );
+      await createTx.wait();
+      const numProposals = await vote.numProposals();
+      expect(numProposals).to.equal(1);
+      const proposal = await vote.getProposal(numProposals.toNumber() - 1);
+      expect(proposal.optionIds).to.deep.equal([]);
+    });
+
+    it("owner can create proposal with multiple options", async function () {
+      const createTx = await vote.createProposal(
+        "test",
+        "testing out proposal",
+        SAMPLE_PROPOSAL
+      );
+      await createTx.wait();
+      const numProposals = await vote.numProposals();
+      // expect(numProposals).to.equal(1);
+      const proposal = await vote.getProposal(numProposals.toNumber() - 1);
+      const optionIds = proposal.optionIds;
+      const options = await vote.getOptions(optionIds);
+      expect(options[0].name).to.equal("a");
+    });
+
+    it("non-owner cannot create a proposal", async function () {
+      const userVote = vote.connect(user1);
+      await expect(
+        userVote.createProposal("test", "testing out proposal", [])
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+  });
+
+  describe("Test voting functions", function () {
+    let user1: SignerWithAddress;
+    let owner: SignerWithAddress;
+    let numProposals: number;
+
+    beforeEach(async function () {
+      [owner, user1] = await ethers.getSigners();
+      const Vote = await ethers.getContractFactory("Vote");
+      vote = (await Vote.deploy()) as Vote;
+      await vote.deployed();
+
+      /* create a proposal */
+
+      const createTx = await vote.createProposal(
+        "test",
+        "testing out proposal",
+        SAMPLE_PROPOSAL
+      );
+      await createTx.wait();
+      const numP = await vote.numProposals();
+      numProposals = numP.toNumber();
+    });
+
+    it("owner can vote on a proposal", async function () {
+      const proposalId = numProposals - 1;
+      const proposal = await vote.getProposal(proposalId);
+      const optionIds = proposal.optionIds;
+      const options = await vote.getOptions(optionIds);
+      const choice = options[0];
+      const voteTx = await vote.vote(choice.proposalId, choice.id);
+      await voteTx.wait();
+      const option = await vote.options(choice.id);
+      expect(option.votes).to.equal(1);
+    });
+
+    it("owner cannot vote twice on a proposal", async function () {
+      const proposalId = numProposals - 1;
+      const proposal = await vote.getProposal(proposalId);
+      const optionIds = proposal.optionIds;
+      const options = await vote.getOptions(optionIds);
+      const choice = options[0];
+      const voteTx = await vote.vote(choice.proposalId, choice.id);
+      await voteTx.wait();
+      /* second vote can't happen */
+      await expect(vote.vote(choice.proposalId, choice.id)).to.be.reverted;
+      const option = await vote.options(choice.id);
+      expect(option.votes).to.equal(1);
+    });
+
+  });
+  // it("user can vote on proposal", async function () {
+
+  // });
+});
