38770866cd8c3cf64fd30a5190e4310202c0eb0e
cha0sg0d <tony.goss@protonmail.com>
2021-12-01 01:01:44 -0500

    final deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,6 +5,19 @@
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
+const SAMPLE_PROPOSAL_OPTIONS = [
+  {
+    name: "ETH",
+    description: "i <3 vitalik",
+    uri: "https://ethereum.org/",
+  },
+  {
+    name: "BTC",
+    description: "i <3 satoshi",
+    uri: "https://nakamotoinstitute.org",
+  },
+];
+
 async function main() {
   const [owner] = await ethers.getSigners();
   const Vote = await ethers.getContractFactory("Vote", owner);
@@ -12,12 +25,13 @@ async function main() {
 
   await vote.deployed();
   console.log("Vote deployed to:", vote.address);
-  const uri =
-    "https://ethresear.ch/t/mev-boost-merge-ready-flashbots-architecture/11177";
-  const createTx = await vote.createProposal(uri);
-
+  const createTx = await vote.createProposal(
+    "ETH or BTC?",
+    "https://ethresear.ch/",
+    SAMPLE_PROPOSAL_OPTIONS
+  );
   await createTx.wait();
-  console.log("Vote deployed to:", vote.address);
+  console.log("Initial proposal created");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
